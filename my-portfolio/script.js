d0d94b38532bfc4899d34684d1b407783675e5c9
rukiii4444 <71225559+rukiii4444@users.noreply.github.com>
2023-11-04 13:04:58 +0900

    Add files via upload

---
+++ my-portfolio/script.js
@@ -0,0 +1,29 @@
+const headerContent = 
+`
+<header>
+    <div class="header-logo">            
+        <a href="https://rukiii4444.github.io/my-portfolio/index.htlm" class="header-logo">haru</a>
+    </div>
+    <div class="header-list">
+        <div class="header-left">
+            <a href="https://rukiii4444.github.io/my-portfolio/blogs/blog.html">Blog</a>
+            <a href="https://rukiii4444.github.io/my-portfolio/contact/contact.html">Contact</a>
+        </div> 
+        <a href="https://github.com/rukiii4444" class="header-right"><i class="fa-brands fa-github"></i>GitHub</a>
+    </div>
+</header>
+`;
+
+document.getElementById('header').innerHTML = headerContent;
+
+const footerContent = 
+`
+<div class="footer-logo">haru</div>
+<div class="footer-list">
+    <ul>
+        <li>Contact</li>
+    </ul>
+</div>
+`
+
+document.getElementById("footer").innerHTML = footerContent;
