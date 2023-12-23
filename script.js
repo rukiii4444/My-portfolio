b83d09c0ec3491409b6050a3c7fb15726ce21cfa
rukiii4444 <71225559+rukiii4444@users.noreply.github.com>
2023-12-23 20:34:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const headerContent =
     </div>
     <div class="header-list">
         <div class="header-left">
-            <a href="/my-portfolio/blogs/blog.html">Blog</a>
+            <a href="/my-portfolio/index.html">Blog</a>
             <a href="my-portfolio/contact/contact.html">Contact</a>
         </div> 
         <a href="https://github.com/rukiii4444" class="header-right"><i class="fa-brands fa-github"></i>GitHub</a>
