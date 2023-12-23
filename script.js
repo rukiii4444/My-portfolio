9b3ccec3c3d9e2182d01e1caa66a01efd3d32727
rukiii4444 <71225559+rukiii4444@users.noreply.github.com>
2023-12-23 20:49:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const headerContent =
 `
 <header>
     <div class="header-logo">            
-        <a href="my-portfolio/index.html" class="header-logo">haru</a>
+        <a href="/my-portfolio/index.html" class="header-logo">haru</a>
     </div>
     <div class="header-list">
         <div class="header-left">
-            <a href="/my-portfolio/index.html">Blog</a>
-            <a href="my-portfolio/contact/contact.html">Contact</a>
+            <a href="/my-portfolio/blogs/blog.html">Blog</a>
+            <a href="/my-portfolio/contact/contact.html">Contact</a>
         </div> 
         <a href="https://github.com/rukiii4444" class="header-right"><i class="fa-brands fa-github"></i>GitHub</a>
     </div>
